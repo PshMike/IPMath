d6d93c2098473837c65c291677f940a72fc691e1
mjenne <pshMike@outlook.com>
2019-06-30 16:37:37 -0500

    Added second param set

--- Get-IPSubnetInfo.ps1
+++ Get-IPSubnetInfo.ps1
@@ -1,163 +1,235 @@
 
 <#
 	.SYNOPSIS
-		Returns basic IP subnet information based on supplied address and mask
+		Returns basic IP subnet information based on supplied NetworkAddress and mask
 	
 	.DESCRIPTION
-		This function will take an IP Address and subnet mask conbination as input and return basic information about the subnet including first and last usable IP addresses
+		This function will take an IP NetworkAddress and subnet mask conbination as input and return basic
+		information about the subnet including first and last usable IP addresses
 	
-	.PARAMETER Address
-		Address in CIDR format ( xxx.xxx.xxx.xxx/yy )
+	.PARAMETER NetworkAddress
+		NetworkAddress in CIDR format ( xxx.xxx.xxx.xxx/yy )
+	
+	.PARAMETER IPAddress
+		A description of the IPAddress parameter.
+	
+	.PARAMETER SubnetMask
+		A description of the SubnetMask parameter.
 	
 	.EXAMPLE
-                PS C:\> Get-IPSubnetInfo -Address 10.152.17.240/22
-
-                Name       : 10.152.16.0/22
-                Network    : 10.152.16.0
-                Mask       : 255.255.252.0
-                MaskLength : 22
-                FirstIP    : 10.152.16.1
-                LastIP     : 10.152.19.254
-                
+		PS C:\> Get-IPSubnetInfo -Address 10.152.17.240/22
+		
+		Address        : 10.152.17.240
+        NetworkName    : 10.152.16.0/22
+        NetworkAddress : 10.152.16.0
+        NetworkMask    : 255.255.252.0
+        MaskLength     : 22
+        FirstIPAddress : 10.152.16.1
+        LastIPAddress  : 10.152.19.254
+	
+	.EXAMPLE
+		PS C:\> Get-IPSubnetInfo -IPAddress 10.152.17.240 -SubnetMask 255.255.252.0
+		
+		Address        : 10.152.17.240
+        NetworkName    : 10.152.16.0/22
+        NetworkAddress : 10.152.16.0
+        NetworkMask    : 255.255.252.0
+        MaskLength     : 22
+        FirstIPAddress : 10.152.16.1
+        LastIPAddress  : 10.152.19.254
+	
+	.OUTPUTS
+		PSObject
 	
 	.NOTES
 		Additional information about the function.
 #>
 function Get-IPSubnetInfo
 {
+    [CmdletBinding(DefaultParameterSetName = 'ByCIDR')]
+    [OutputType([psobject], ParameterSetName = 'ByCIDR')]
+    [OutputType([psobject], ParameterSetName = 'ByIPandMask')]
     [OutputType([psobject])]
     param
     (
-        [string[]]$Address
+        [Parameter(ParameterSetName = 'ByCIDR',
+            Mandatory = $true)]
+        [string[]]$Address,
+        [Parameter(ParameterSetName = 'ByIPandMask',
+            Mandatory = $true)]
+        [IPAddress]$IPAddress,
+        [Parameter(ParameterSetName = 'ByIPandMask',
+            Mandatory = $true)]
+        [IPAddress]$SubnetMask
     )
-
-   
 	
     begin
-    { }
-	
+    {
+    
+    }
+
     process
     {
+        <# 
+        There is no built in -BAND -BOR -BXOR operators for [Byte[]] so this function is needed
+        
+        For instance, network address = IPAddress -BAND MaskAddress  
+        #>		
         Function ByteArrayOperation
         {
-            param 
+            param
             (
                 [byte[]]$array1,
                 [byte[]]$array2,
                 [string]$Operation
-            
             )
-    
-            $ba3 = [System.Byte[]]::new($array1.Length) 
-    
-            for ($i = 0; $i -lt $ba3.Length; $i++)
+			
+            $ResultByteArray = [System.Byte[]]::new($array1.Length)
+			
+            for ($i = 0; $i -lt $ResultByteArray.Length; $i++)
             {
-                if ($Operation -eq 'BAND') { $ba3[$i] = $array1[$i] -band $array2[$i] }
-                if ($Operation -eq 'BXOR') { $ba3[$i] = $array1[$i] -bxor $array2[$i] }
-                if ($Operation -eq 'BOR') { $ba3[$i] = $array1[$i] -bor $array2[$i] }
+                if ($Operation -eq 'BAND') { $ResultByteArray[$i] = $array1[$i] -band $array2[$i] }
+                if ($Operation -eq 'BXOR') { $ResultByteArray[$i] = $array1[$i] -bxor $array2[$i] }
+                if ($Operation -eq 'BOR') { $ResultByteArray[$i] = $array1[$i] -bor $array2[$i] }
             }
-    
-            $ba3
+			
+            $ResultByteArray
         }
-    
-        foreach ($SingleAddress in $Address)
+		
+        switch ($pscmdlet.ParameterSetName)
         {
-
-            #Validate IP Address is valid
-            try
+            'ByCIDR'
             {
-                $AddressIP = [ipaddress]::Parse($SingleAddress.Split('/')[0])
-                $ParametersValidated = $true 
-            }
-            catch
-            {
-                '{0} is invalid IP Address' -f $SingleAddress | Write-Error
-                $ParametersValidated = $false 
-            }
-
-            try
-            {
-                $MaskLength = [int]($SingleAddress.Split('/')[1])
-                $ParametersValidated = $ParametersValidated -and $true 
-            }
-            catch
-            {
-                '{0} has invalid mask length' -f $SingleAddress | Write-Error
-                $ParametersValidated = $false
-            }
-
-            if ($masklength -lt 1)
-            {
-                $ParametersValidated = $false
-                '{0} has mask length too small' -f $SingleAddress | Write-Error
-            }
-
-            if ($AddressIP.AddressFamily -eq 'InterNetworkV6' -and $masklength -gt 126)
-            {
-                $ParametersValidated = $false
-                '{0} has mask length too large for IPv6' -f $SingleAddress | Write-Error
-            }
-
-            if ($ParametersValidated)
-            {
-                $MaskBytes = [byte[]]::new($AddressIP.GetAddressBytes().Length)
-            
-                $i = 0
-                do
+                foreach ($SingleAddress in $Address)
                 {
-                    if ($maskLength -ge ((1 + $i) * 8)) 
-                    { 
-                        $MaskBytes[$i] = 255
-                    }
-                    else
+                    try
                     {
-                        $MaskBytes[$i] = (255 -shl ((1 + $i) * 8) - $maskLength) % 256
+                        $AddressIP = [ipaddress]::Parse($SingleAddress.Split('/')[0])
+                        $ParametersValidated = $true
                     }
-            
-                    $i++
-                } while ($maskLength -gt ($i * 8))
-            
-                $MaskAddress = [IPAddress]::new($MaskBytes)
-			
-                $NetworkAddress = [ipaddress]::new((ByteArrayOperation $AddressIP.GetAddressBytes() $MaskAddress.GetAddressBytes() -Operation BAND))
-			
+                    catch
+                    {
+                        '{0} is invalid IP Address' -f $SingleAddress | Write-Error
+                        $ParametersValidated = $false
+                    }
+                    
+                    try
+                    {
+                        $MaskLength = [int]($SingleAddress.Split('/')[1])
+                        $ParametersValidated = $ParametersValidated -and $true
+                    }
+                    catch
+                    {
+                        '{0} has invalid mask length' -f $SingleAddress | Write-Error
+                        $ParametersValidated = $false
+                    }
+                    
+                    if ($masklength -lt 1)
+                    {
+                        $ParametersValidated = $false
+                        '{0} has mask length too small' -f $SingleAddress | Write-Error
+                    }
+                    
+                    if ($ParametersValidated)
+                    {
+                        $MaskBytes = [byte[]]::new($AddressIP.GetAddressBytes().Length)
+                        
+                        $i = 0
+                        do
+                        {
+                            if ($maskLength -ge ((1 + $i) * 8))
+                            {
+                                $MaskBytes[$i] = 255
+                            }
+                            else
+                            {
+                                $MaskBytes[$i] = (255 -shl ((1 + $i) * 8) - $maskLength) % 256
+                            }
+                            
+                            $i++
+                        }
+                        while ($maskLength -gt ($i * 8))
+                        
+                        $MaskAddress = [IPAddress]::new($MaskBytes)
+                        
+                        $NetworkBytes = ByteArrayOperation $AddressIP.GetAddressBytes() $MaskBytes -Operation BAND
+                        $NetworkAddress = [ipaddress]::new($NetworkBytes)
+                        
+                        if ($MaskBytes[-1] -le 252)
+                        {
+                            $FirstIPBytes = $NetworkAddress.GetAddressBytes()
+                            $FirstIPBytes[-1] += 1
+                            $FirstIPAddress = [ipaddress]::new($FirstIPBytes)
+                            
+                            $BroadcastBytes = [System.Byte[]]::new($FirstIPBytes.Length)
+                            for ($i = 0; $i -lt $BroadcastBytes.Length; $i++) { $BroadcastBytes[$i] = 255 }
+                            
+                            $MaskBytes = $MaskAddress.GetAddressBytes()
+                            
+                            $HostBytes = ByteArrayOperation $MaskBytes $BroadcastBytes -Operation BXOR
+                            $LastIPBytes = ByteArrayOperation $NetworkBytes $HostBytes -Operation BOR
+                            
+                            $LastIPBytes[-1] -= 1
+                            
+                            $LastIPAddress = [ipaddress]::new($LastIPBytes)
+                        }
+                        else
+                        {
+                            $FirstIPAddress = $null
+                            $LastIPAddress = $null
+                        }
+                        
+                        $obj = [pscustomobject][ordered] @{
+                            Address        = $AddressIP
+                            NetworkName    = $NetworkAddress.ToString() + '/' + $masklength
+                            NetworkAddress = $NetworkAddress
+                            NetworkMask    = $MaskAddress
+                            MaskLength     = $masklength
+                            FirstIPAddress = $FirstIPAddress
+                            LastIPAddress  = $LastIPAddress
+                        }
+                        
+                        $obj
+                    }
+                }        
+            }
 
-                if ($MaskBytes[-1] -le 252)
+            'ByIPandMask'
+            {
+                # create CIDR address and call into self
+
+                $MaskBytes = $SubnetMask.GetAddressBytes()
+
+                $MaskLength = 0
+
+
+                for ($i = 0; $i -lt $MaskBytes.Length; $i++)
                 {
-                    $FirstIPByteArray = $NetworkAddress.GetAddressBytes()
-                    $FirstIPByteArray[-1] += 1
-                    $FirstIPAddress = [ipaddress]::new($FirstIPByteArray)
-			
-                    $BroadcastIPByteArray = [System.Byte[]]::new($FirstIPByteArray.Length)
-                    for ($i = 0; $i -lt $BroadcastIPByteArray.Length; $i++) { $BroadcastIPByteArray[$i] = 255 }
-			
-                    $NetworkByteArray = $NetworkAddress.GetAddressBytes()
-                    $MaskByteArray = $MaskAddress.GetAddressBytes()
-			
-                    $HostByteArray = ByteArrayOperation $MaskByteArray $BroadcastIPByteArray -Operation BXOR
-                    $LastIPByteArray = ByteArrayOperation $NetworkByteArray $HostByteArray -Operation BOR
-			
-                    $LastIPByteArray[-1] -= 1
-			
-                    $LastIPAddress = [ipaddress]::new($LastIPByteArray)
-                }
-                else
-                {
-                    $FirstIPAddress = $null
-                    $LastIPAddress = $null
+                    if ($MaskBytes[$i] -eq 255) 
+                    { 
+                        $MaskLength += 8 
+                    }
+                    elseif ($MaskBytes[$i] -gt 0)
+                    {
+                        $MaskLength += 8 - [Math]::Log((1 + ($MaskBytes[$i] -bxor 255)), 2)
+                    }    
                 }
 
-                $obj = [pscustomobject][ordered] @{
-                    Address        = $AddressIP.ToString()
-                    NetworkName    = $NetworkAddress.ToString() + '/' + $masklength
-                    NetworkAddress = $NetworkAddress.ToString()
-                    NetworkMask    = $MaskAddress.ToString()
-                    MaskLength     = $masklength
-                    FirstIPAddress = $FirstIPAddress
-                    LastIPAddress  = $LastIPAddress
+                try
+                {
+                    $FinalMaskLength = [int]::Parse($MaskLength)
                 }
-			
-                $obj
+                catch
+                {
+                    '{0} is not a valid subnet mask' -f $SubnetMask.ToString() | Write-Error
+                }
+
+                if ($FinalMaskLength)
+                {
+                    $CIDR = $IPAddress.ToString() + '/' + $FinalMaskLength
+
+                    Get-IPSubnetInfo -Address $CIDR 
+                }
             }
         }
     }
@@ -167,3 +239,7 @@ function Get-IPSubnetInfo
 		
     }
 }
+
+
+
+
